619893a53db380aceee44f4822c3180d2e1b79f1
huckjuhwang <skygurwn96@naver.com>
2023-12-03 16:10:40 +0900

    [HJ] chapter6, 기본 구조 셋팅

---
+++ src/Chapter6/hj/basic/Client.java
@@ -0,0 +1,15 @@
+package Chapter6.hj.basic;
+
+public class Client {
+
+
+    public static void main(String[] args) {
+        Invoker invoker = new Invoker();
+
+        Receiver receiver = new Receiver();
+        ConcreteCommend commend = new ConcreteCommend(receiver);
+
+        invoker.setCommend(commend);
+        invoker.buttonClick();
+    }
+}
